e33af20a45a4d844527fce4ce4a69c1026696213
AlanYen <alanyen.mobile@gmail.com>
2015-09-18 16:21:12 +0800

    Initial Commit

---
+++ TryAnimationControlByTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TryAnimationControlByTimer
+//
+//  Created by AlanYen on 2015/9/18.
+//  Copyright © 2015年 17Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
